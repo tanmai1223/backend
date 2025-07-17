3f31b073d0c020105792d895a5cb04c9195e6560
tanmai1223 <htanmai23@gmail.com>
2025-07-17 14:22:40 +0530

    17-07-25

--- mongoDB/routes/user.js
+++ mongoDB/routes/user.js
@@ -12,4 +12,51 @@ routers.post("/", async (req, res) => {
   }
 });
 
-module.exports=routers;
+routers.get("/", async (req, res) => {
+  try {
+    const user = await User.find();
+    res.send(user);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+routers.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+routers.put("/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+routers.delete("/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send("USer not found");
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+module.exports = routers;
